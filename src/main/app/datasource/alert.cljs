9a0a0b46ed689fe74b72c0c6a6ee5a8a6910bb8d
Michael Torres <torresmi@mail.gvsu.edu>
2020-06-06 21:29:23 -0400

    add alert specification

---
+++ src/main/app/datasource/alert.cljs
@@ -0,0 +1,19 @@
+(ns app.datasource.alerts
+  (:require
+   [app.datasource.game :as game]
+   [app.domain.specs :refer [email?]]
+   [cljs.spec.alpha :as s]))
+
+(s/def ::edit-action #{"set" "delete"})
+(s/def ::email (s/and string? email?))
+(s/def ::price (s/and pos? float?))
+
+(s/def ::edit-alert (s/keys :req [::edit-action
+                                  ::email
+                                  ::game/id
+                                  ::price]))
+
+(s/def ::manage-action #(= "manage" %))
+
+(s/def ::manage-request (s/keys :req [::manage-action
+                                      ::email]))
--- src/main/app/domain/predicates.cljs
+++ src/main/app/domain/predicates.cljs
@@ -3,3 +3,6 @@
    [clojure.string :as string]))
 
 (def not-blank? (comp not string/blank?))
+
+(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
+(def email? #(re-matches email-regex %));
